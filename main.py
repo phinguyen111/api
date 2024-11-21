8d025dfb7ed1fc30e9f2a2a62307a69fb9eefd3a
phinguyen111 <125988580+phinguyen111@users.noreply.github.com>
2024-11-21 13:13:33 +0700

    Update main.py lần 2

--- main.py
+++ main.py
@@ -44,12 +44,12 @@ def home():
 def get_transactions():
     """Fetch transactions for a given address."""
     if request.method == 'OPTIONS':
-        # Phản hồi preflight request
-        response = make_response(jsonify({"message": "Preflight request successful"}))
+        response = make_response()
         response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"]
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Requested-With"]
         return response
+
     start_time = time.time()  # Bắt đầu theo dõi thời gian xử lý
 
     try:
