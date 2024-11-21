e9f32659ddc65493e255ccc197077cfcbae9bad9
phinguyen111 <125988580+phinguyen111@users.noreply.github.com>
2024-11-21 16:11:23 +0700

    Update etherscan_to_neo4j.py fix ve cu

--- etherscan_to_neo4j.py
+++ etherscan_to_neo4j.py
@@ -2,7 +2,6 @@ from database_connection import Neo4jConnection
 import requests
 import time
 import os
-import asyncio
 
 uri = os.getenv("NEO4J_URI", "neo4j+s://aadff3f9.databases.neo4j.io")
 username = os.getenv("NEO4J_USER", "neo4j")
@@ -10,7 +9,7 @@ password = os.getenv("NEO4J_PASSWORD", "TVuvrmUqxBe3u-gDv6oISHDlZKLxUJKz3q8FrOXy
 
 neo4j_connection = Neo4jConnection(uri, username, password)
 
-async def fetch_and_save_transactions(address, limit=25):
+def fetch_and_save_transactions(address, limit=25):
     etherscan_api_key = os.getenv("ETHERSCAN_API_KEY", "IGVQMMEFYD8K2DK22ZTFV6WK1RH8KP98IS")
     url = f"https://api.etherscan.io/api?module=account&action=txlist&address={address}&startblock=0&endblock=99999999&sort=desc&apikey={etherscan_api_key}"
 
@@ -24,7 +23,6 @@ async def fetch_and_save_transactions(address, limit=25):
             transactions = data["result"][:limit]  # Fetch limit transactions
             print(f"Fetched {len(transactions)} transactions in {time.time() - start_time:.2f} seconds")
             neo4j_start_time = time.time()  # Thời gian bắt đầu lưu vào Neo4j
-            
             for tx in transactions:
                 if int(tx['value']) > 0:
                     # Convert wei to ether
@@ -43,12 +41,8 @@ async def fetch_and_save_transactions(address, limit=25):
                     MERGE (t)-[:RECEIVED_FROM {{amount: {amount_in_ether}, timestamp: {tx['timeStamp']}}}]->(r)
                     """
                     neo4j_connection.execute_query(query)
-                    print(f"Saved transaction {tx['hash']} to Neo4j")
-                    
-                    # Chờ 0.2 giây giữa các lần xử lý để tuân thủ giới hạn 5 API calls/sec
-                    await asyncio.sleep(0.2)
-            
-            print(f"Saved all transactions to Neo4j in {time.time() - neo4j_start_time:.2f} seconds")
+                    print(f"Saved transactions to Neo4j in {time.time() - neo4j_start_time:.2f} seconds")
+
             return transactions
         else:
             print(f"No transactions found for address {address}")
@@ -56,12 +50,3 @@ async def fetch_and_save_transactions(address, limit=25):
     except Exception as e:
         print(f"Error fetching transactions: {e}")
         return []
-
-# Hàm gọi async
-async def main():
-    address = "0xYourAddressHere"
-    await fetch_and_save_transactions(address)
-
-# Chạy hàm bất đồng bộ
-if __name__ == "__main__":
-    asyncio.run(main())
